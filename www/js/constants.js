84eab6c9376da000687ef75c7e2ca53ac60fdb17
Slava Elantsev <slvlntsv@gmail.com>
2015-09-13 22:02:36 +0200

    new images

--- www/js/constants.js
+++ www/js/constants.js
@@ -1,5 +1,5 @@
 angular.module('ComparePrices.constants', []).constant('ComparePricesConstants', {
-        'DEFAULT_IMAGE_URL': "img/tabasco.jpg",
+        'DEFAULT_IMAGE_URL': "img/user_cart.jpg",
         'DEFAULT_MAX_SHOPS_TO_SHOW': 10,
         'DEFAULT_MAX_SHOPS_OF_THE_SAME_BRAND': 3,
         'DEFAULT_SHOPS_RANGE': 5,
